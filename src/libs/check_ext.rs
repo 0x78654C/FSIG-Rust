43934348b99972828c610259c12cb35471bec263
0x78654c <xcoding.dev@gmail.com>
2024-06-11 03:32:52 +0300

    fix check case sensitivity

--- src/libs/check_ext.rs
+++ src/libs/check_ext.rs
@@ -4,9 +4,10 @@ use std::fs::read_to_string;
 pub fn check_ext(filename: &str, hex_line:&str, ext_file:&str) {
     for line in read_to_string(ext_file).unwrap().lines() {
         let split:Vec<_> = line.split("|").collect();
-        if hex_line.contains(&split[0]) {
+        let split_low =&split[0].to_ascii_lowercase(); 
+        if hex_line.to_ascii_lowercase().starts_with(split_low) {
             let output = "
-            -------------------------------------------------------------	
+-------------------------------------------------------------	
 File:          ".to_owned()+filename+"
 Extension(s):  "+&split[0]+"
 Hex signature: "+&split[1]+"
